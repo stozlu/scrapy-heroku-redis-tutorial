d54ca4eb4f17bf88bc1aa120350e3a2f63723793
Seckin Tozlu <stozlu@constantcontact.com>
2015-01-02 20:33:53 -0500

    add redis settings, comment out heroku config

--- scrapy_heroku_redis_tutorial/settings.py
+++ scrapy_heroku_redis_tutorial/settings.py
@@ -13,13 +13,15 @@ BOT_NAME = 'scrapy_heroku_redis_tutorial'
 SPIDER_MODULES = ['scrapy_heroku_redis_tutorial.spiders']
 NEWSPIDER_MODULE = 'scrapy_heroku_redis_tutorial.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'scrapy_heroku_redis_tutorial (+http://www.yourdomain.com)'
-
 # Store scraped item in redis for post-processing.
-ITEM_PIPELINES = [
-    'scrapy_redis.pipelines.RedisPipeline',
-]
+ITEM_PIPELINES = {
+    'scrapy_redis.pipelines.RedisPipeline': 999,
+}
 
-# Specify the full Redis URL for connecting
-REDIS_URL = 'redis://user:pass@hostname:9001'
+# Specify the host and port to use when connecting to Redis (optional).
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+# Specify the full Redis URL for connecting (optional).
+# If set, this takes precedence over the REDIS_HOST and REDIS_PORT settings.
+# REDIS_URL = 'redis://user:pass@hostname:9001'
